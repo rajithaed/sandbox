e86630f06ab2c001ba6cb771b7226ca3bb474ba4
Rajitha Edirisinghe <rajitha.p.edirisinghe@pwc.com>
2018-11-19 14:32:45 +0000

    initial commit with proj files and gitignore

---
+++ my.sandbox.tests/my.sandbox.tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+
+namespace my.sandbox.tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Given_When_Then()
+        {
+            //arrange
+
+            //act
+
+            //assert
+            Assert.True(true);
+        }
+    }
+}
